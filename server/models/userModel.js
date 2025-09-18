674eba445c0eeaf10577871a0cc3201f39fffc1f
Bar Larrea <barlarrea1@gmail.com>
2025-09-18 16:30:37 +0300

    feture/created user model

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        hashedPassword: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        profiles: {
+            type: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Profile"
+                }
+            ],
+            validate: {
+                validator: function (arr) {
+                    return arr.length <= 5;
+                },
+                message: "A user can have at most 5 profiles"
+            }
+        }
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
