fabe9e9087aa5b17c041afffd7a9df447b4f5351
Bar Larrea <barlarrea1@gmail.com>
2025-09-24 15:42:49 +0300

    add refreshToken schema to user model (include session id)

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,28 @@
 import mongoose from "mongoose";
+import { type } from "os";
+
+// Sub-schema for refresh tokens (each represents one active session)
+const refreshTokenSchema = new mongoose.Schema(
+    {
+        token: {
+            type: String,
+            required: true
+        },
+        jti: {
+            type: String,
+            required: true
+        }, // Unique identifier for the session (Session ID)
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }, // When the session was created
+        lastUsed: {
+            type: Date,
+            default: Date.now
+        } // Last time the refresh token was used
+    },
+    { _id: false }
+);
 
 const userSchema = new mongoose.Schema(
     {
@@ -35,6 +59,16 @@ const userSchema = new mongoose.Schema(
                     return arr.length <= 5;
                 },
                 message: "A user can have at most 5 profiles"
+            }
+        },
+        refreshTokens: {
+            type: [refreshTokenSchema],
+            validate: {
+                validator: function (arr) {
+                    return arr.length <= 5;
+                },
+                message:
+                    "A user can be logged in on a maximum of 5 devices simultaneously"
             }
         }
     },
