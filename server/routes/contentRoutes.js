b686290e197c6457325cd66d1ea397c4bbd8a67a
Bar Larrea <barlarrea1@gmail.com>
2025-10-16 20:27:37 +0300

    Add search and season/episode routes to content API

--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -5,14 +5,24 @@ import checkUserStatus from "../middlewares/userStatusMiddleware.js";
 
 import {
     getAllContents,
-    getContentById
+    getContentById,
+    searchContents,
+    getSeasonsBySeriesId,
+    getEpisodesBySeasonId
 } from "../controllers/contentController.js";
 
 const router = express.Router();
 
 const contentPipeline = [verifyAccessToken, checkUserStatus];
 
+router.get("/search", contentPipeline, searchContents);
 router.get("/", contentPipeline, getAllContents);
 router.get("/:id", contentPipeline, getContentById);
+router.get("/series/:seriesId/seasons", contentPipeline, getSeasonsBySeriesId);
+router.get(
+    "/seasons/:seasonId/episodes",
+    contentPipeline,
+    getEpisodesBySeasonId
+);
 
 export default router;
