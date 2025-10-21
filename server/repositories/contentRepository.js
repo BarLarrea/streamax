1e40183812b371cac9b0b0fd546540769613b2bf
Bar Larrea <barlarrea1@gmail.com>
2025-10-21 11:43:29 +0300

    Refactor ID validation to use isValidId utility function

--- server/repositories/contentRepository.js
+++ server/repositories/contentRepository.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import Content from "../models/contentModel.js";
+import { isValidId } from "../utils/dalUtils.js";
 
 // ==================== CREATE / UPDATE / DELETE ====================
 
@@ -27,7 +28,7 @@ export const deleteContent = async (id) => {
 // ==================== READ / QUERY / SEARCH ====================
 
 export const getContentById = async (contentId) => {
-    if (!mongoose.Types.ObjectId.isValid(contentId)) {
+    if (!isValidId(contentId)) {
         return { status: "invalid_id", data: null };
     }
 
@@ -108,7 +109,7 @@ export const searchContents = async (query, limit, skip) => {
 // ==================== HIERARCHY ====================
 
 export const getSeasonsBySeriesId = async (seriesId) => {
-    if (!mongoose.Types.ObjectId.isValid(seriesId)) {
+    if (!isValidId(seriesId)) {
         return { status: "invalid_id", data: null };
     }
 
@@ -124,7 +125,7 @@ export const getSeasonsBySeriesId = async (seriesId) => {
 };
 
 export const getEpisodesBySeasonId = async (seasonId) => {
-    if (!mongoose.Types.ObjectId.isValid(seasonId)) {
+    if (!isValidId(seasonId)) {
         return { status: "invalid_id", data: null };
     }
 
