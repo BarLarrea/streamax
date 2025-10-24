4b18942478d2366c3ffbcf7d647a08e92e3f9bee
Bar Larrea <barlarrea1@gmail.com>
2025-10-24 13:49:00 +0300

    Add home page HTML and CSS, update component loading functions

--- frontend/utils/loadComponent.js
+++ frontend/utils/loadComponent.js
@@ -1,9 +1,10 @@
 const loadComponent = async (selector, htmlPath, cssPath) => {
     try {
-        if (!selector || !htmlPath || !cssPath) {
-            throw new Error("Selector and HTML path and CSS path are required");
+        if (!selector || !htmlPath) {
+            throw new Error("Selector and HTML path are required");
         }
         const html = await fetch(htmlPath).then((res) => res.text());
+
         document.querySelector(selector).innerHTML = html;
 
         if (cssPath) {
--- frontend/utils/loadPage.js
+++ frontend/utils/loadPage.js
@@ -1,5 +1,8 @@
 const loadPage = async (htmlPath, cssPath) => {
     try {
+        if (!htmlPath) {
+            throw new Error("HTML path is required");
+        }
         const html = await fetch(htmlPath).then((res) => res.text());
         const main = document.querySelector("#app-main");
         main.innerHTML = html;
