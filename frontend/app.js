d8dd84822e02c66627e921494a25dd7e5c02d7aa
Bar Larrea <barlarrea1@gmail.com>
2025-10-24 13:08:23 +0300

    Refactor app.js to modularize component loading and simplify initialization logic

--- frontend/app.js
+++ frontend/app.js
@@ -1,54 +1,12 @@
-async function loadComponent(selector, htmlPath, cssPath) {
-    try {
-        if (!selector || !htmlPath || !cssPath) {
-            throw new Error("Selector and HTML path and CSS path are required");
-        }
-        const html = await fetch(htmlPath).then((res) => res.text());
-        document.querySelector(selector).innerHTML = html;
+import loadHeader from "./shared/components/header/index.js";
+import loadFooter from "./shared/components/footer/index.js";
+import loadPage from "./utils/loadPage.js";
 
-        if (cssPath) {
-            const link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = cssPath;
-            document.head.appendChild(link);
-        }
-    } catch (err) {
-        console.error(`Error loading component: ${htmlPath}`, err);
-    }
-}
-
-async function loadPage(htmlPath, cssPath) {
-    try {
-        const html = await fetch(htmlPath).then((res) => res.text());
-        const main = document.querySelector("#app-main");
-        main.innerHTML = html;
-
-        if (cssPath) {
-            const link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = cssPath;
-            document.head.appendChild(link);
-        }
-
-        console.log(`Loaded page: ${htmlPath}`);
-    } catch (err) {
-        console.error(`Error loading page ${htmlPath}`, err);
-    }
-}
-
-async function initApp() {
-    await loadComponent(
-        "#app-header",
-        "shared/components/header/header.html",
-        "shared/components/header/header.css"
-    );
-    await loadComponent(
-        "#app-footer",
-        "./shared/components/footer/footer.html",
-        "./shared/components/footer/footer.css"
-    );
+const initApp = async () => {
+    await loadHeader();
+    await loadFooter();
 
     await loadPage("./pages/home/home.html", "./pages/home/home.css");
-}
+};
 
 document.addEventListener("DOMContentLoaded", initApp);
