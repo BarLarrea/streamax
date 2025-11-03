8530ce3cf53a56a33c0af25518f818ea9bda4b83
Bar Larrea <barlarrea1@gmail.com>
2025-11-03 16:15:51 +0200

    Update cookie settings for refresh token

--- backend/src/utils/cookie.js
+++ backend/src/utils/cookie.js
@@ -1,18 +1,20 @@
+const isLocal = process.env.NODE_ENV !== "production";
+
 export function setUpRefreshTokenCookie(res, refreshToken) {
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // HTTPS only in production
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "none", //  allow cross-origin in dev
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        path: "/" // minst be sent for all routes
+        secure: true, // always true
+        sameSite: isLocal ? "none" : "strict",
+        path: "/",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
 }
 
 export function clearRefreshTokenCookie(res) {
     res.clearCookie("refreshToken", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
+        secure: true,
+        sameSite: isLocal ? "none" : "strict",
         path: "/"
     });
 }
