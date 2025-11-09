6120a172d916536b8293f1075f28c09062825149
Bar Larrea <barlarrea1@gmail.com>
2025-11-10 00:07:11 +0200

    Update header logic and refine styles for auth and profiles pages

--- frontend/shared/components/header/header.js
+++ frontend/shared/components/header/header.js
@@ -2,8 +2,13 @@ import loadNavBar from "../navBar/index.js";
 import loadUserMenu from "../userMenu/index.js";
 
 export const initAppHeader = async (currentPage) => {
-    const nonNavPages = ["profiles", "create-profile"];
-    const userMenuPages = ["profiles", "create-profile"]; // mabe will add more pages later
+    const nonNavPages = ["profiles", "create-profile", "edit-user"];
+    const userMenuPages = [
+        "profiles",
+        "create-profile",
+        "edit-user",
+        "notFound"
+    ]; // mabe will add more pages later
 
     if (!nonNavPages.includes(currentPage)) {
         await loadNavBar();
